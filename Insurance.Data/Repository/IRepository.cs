9be39f4cf6c3a0dea624773d6facd09491bb4fb6
lfaletti <luciano.faletti@hotmail.com>
2018-08-02 21:53:10 +0200

    Add Data project.

---
+++ Insurance.Data/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Insurance.Data.Repository
+{
+    /// <summary>
+    /// Repository with generic contract.
+    /// </summary>
+    public interface IRepository<T>
+    {
+        IEnumerable<T> Get();
+        IEnumerable<T> Get(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
---
+++ Insurance.Data/UoW/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Insurance.Data.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        public DbContext Context { get; }
+
+        public UnitOfWork(DbContext context)
+        {
+            Context = context;
+        }
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+
+        }
+    }
+}
